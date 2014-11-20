2d18be6494553f4d42ad3a19fee6a584c6cd335e
vandelee000 <vandelee000@RIV803-35318.asd.auburn.wednet.edu>
2014-11-20 08:26:54 -0800

    update

--- PasswordGen/src/cups/classfiles.java
+++ PasswordGen/src/cups/classfiles.java
@@ -1,5 +1,24 @@
 package cups;
 
-public class classfiles {
-
+import java.awt.Color;
+import javax.swing.JOptionPane;
+public class classfiles extends Turtle{
+	
+	
+	public void makeCup(){
+		switchTo(Color.RED);
+		fillBox(25,50);
+	}
+	public String returnMove(){
+		String Str = JOptionPane.showInputDialog("up/down/left/up");
+		
+		System.out.println(Str);
+		
+		return Str;
+		
+	}
+	public void hand(String move){
+		
+	}
+	
 }
--- PasswordGen/src/cups/cupmain.java
+++ PasswordGen/src/cups/cupmain.java
@@ -3,6 +3,13 @@ package cups;
 
 public class cupmain {
 	public static void main(String[] args) {
+		Turtle cups=new Turtle();
+		classfiles cups2=new classfiles();
+		//cups2.makeCup();
+		cups2.returnMove();
+		
+		
+		
 		
 	}
 }
