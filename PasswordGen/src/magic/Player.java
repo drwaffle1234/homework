c528501fc71dc64cb811166c8524a9d9a26cc6c6
Vandelee000 <Vandelee000@RIV-35318.asd.auburn.wednet.edu>
2014-12-09 08:55:49 -0800

    import stuff

--- PasswordGen/src/magic/Player.java
+++ PasswordGen/src/magic/Player.java
@@ -1,21 +1,12 @@
 package magic;
 import java.awt.Button;
-import java.awt.Checkbox;
 import java.awt.Color;
-import java.awt.TextArea;
 import java.awt.TextField;
-import java.awt.CheckboxGroup;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.JProgressBar;
 import javax.swing.JApplet;
-import javax.swing.JFrame;
-import javax.swing.border.Border;
-import javax.swing.SwingWorker;
+import javax.swing.JOptionPane;
 public class Player {
 	private int setHealth=20;
 	private int maxHealth=200;
