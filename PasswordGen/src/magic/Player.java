a9cf22c792bad93fd1d46b85cd34876917507fc5
Vandelee000 <Vandelee000@RIV-35318.asd.auburn.wednet.edu>
2014-12-09 08:40:58 -0800

    added imports

--- PasswordGen/src/magic/Player.java
+++ PasswordGen/src/magic/Player.java
@@ -1,5 +1,21 @@
 package magic;
+import java.awt.Button;
+import java.awt.Checkbox;
+import java.awt.Color;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.CheckboxGroup;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JProgressBar;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.border.Border;
+import javax.swing.SwingWorker;
 public class Player {
 
 }
